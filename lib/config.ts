a3a4051c3a9b5b12a99e209e2c4cc8572f1f6106
Josman Proudinat <jmps2812@gmail.com>
2022-03-23 21:05:21 -0600

    Sitemap.xml

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+export const isDev =
+  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+
+export const port = '3000'
+export const domain = 'www.josmanproudinat.com'
+
+export const host = isDev ? `http://localhost:${port}` : `https://${domain}`
---
+++ pages/api/sitemap.xml.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { host } from '../../lib/config'
+import { getAllPosts } from '../../lib/api'
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (req.method !== 'GET') {
+    return res.status(405).send({ error: 'method not allowed' })
+  }
+
+  const posts = getAllPosts(['slug'])
+
+  // cache sitemap for up to one hour
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=3600, max-age=3600, stale-while-revalidate=3600'
+  )
+  res.setHeader('Content-Type', 'text/xml')
+
+  res.write(createSitemap(posts))
+  res.end()
+}
+
+const createSitemap = (posts: any[]) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${host}</loc>
+      </url>
+
+      <url>
+        <loc>${host}/</loc>
+      </url>
+
+      ${posts
+        .map((post) =>
+          `
+            <url>
+              <loc>${host}/posts/${post.slug}</loc>
+            </url>
+          `.trim()
+        )
+        .join('')}
+    </urlset>
+    `
