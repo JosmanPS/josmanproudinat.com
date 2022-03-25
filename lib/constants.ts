f771960f3e1b58a51168d10192d2b9d1a9c1038f
Josman Proudinat <jmps2812@gmail.com>
2022-03-25 16:00:54 -0600

    Cambio de imagen meta

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ import { host } from "./config"
 
 export const EXAMPLE_PATH = 'blog-starter-typescript'
 export const CMS_NAME = 'Markdown'
-export const HOME_OG_IMAGE_URL = `${host}/img/josman-bg-01.jpeg`
+export const HOME_OG_IMAGE_URL = `${host}/img/josman-bg-01.jpg`
