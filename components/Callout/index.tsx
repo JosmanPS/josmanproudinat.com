4b6cc37a2dbb9fc5c663cb0b7ae577a3c0b85e37
Josman Proudinat <jmps2812@gmail.com>
2022-03-24 20:08:22 -0600

    Redondeo y margen en Callout

--- components/Callout/index.tsx
+++ components/Callout/index.tsx
@@ -9,11 +9,12 @@ export default function Callout({ children, type }: Props) {
   const bgColors: { [key: string]: string } = {
     default: 'bg-gray-100',
     idea: 'bg-amber-50',
+    info: 'bg-blue-50',
   }
 
   const bgColor = !!type
     ? bgColors[type] || bgColors['default']
     : bgColors['default']
 
-  return <div className={`px-8 py-4 ${bgColor}`}>{children}</div>
+  return <div className={`rounded-md px-8 py-4 mb-4 ${bgColor}`}>{children}</div>
 }
