354967fb343009a5759296e4459f512ecddcd8bb
Josman Proudinat <jmps2812@gmail.com>
2022-04-04 12:07:43 -0600

    Boton de mostrar mas posts

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import PostPreview from './post-preview'
 import Post from '../types/post'
 
@@ -6,13 +7,33 @@ type Props = {
 }
 
 const MoreStories = ({ posts }: Props) => {
+  const [postsShowing, setPostsShowing] = useState<Post[]>([])
+  const [lastPostIndex, setLastPostIndex] = useState(0)
+
+  useEffect(() => {
+    const initialPosts = posts.slice(0, 4)
+    setPostsShowing(initialPosts)
+    setLastPostIndex(initialPosts.length - 1)
+  }, [])
+
+  const onShowMore = () => {
+    if (posts.length === lastPostIndex - 1) {
+      return
+    }
+
+    const newPosts = posts.slice(lastPostIndex + 1, lastPostIndex + 4)
+    const postsToShow = [...postsShowing, ...newPosts]
+    setPostsShowing(postsToShow)
+    setLastPostIndex(postsToShow.length - 1)
+  }
+
   return (
     <section className="py-8">
       <h2 className="mb-8 text-3xl md:text-4xl font-bold tracking-tighter leading-tight">
         Más reciente
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        {posts.map((post) => (
+        {postsShowing.map((post) => (
           <PostPreview
             key={post.slug}
             title={post.title}
@@ -23,6 +44,16 @@ const MoreStories = ({ posts }: Props) => {
             excerpt={post.excerpt}
           />
         ))}
+      </div>
+      <div>
+        {lastPostIndex + 1 < posts.length && (
+          <button
+            className="bg-transparent border border-black hover:bg-black hover:text-white w-full font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-12"
+            onClick={onShowMore}
+          >
+            Ver más posts
+          </button>
+        )}
       </div>
     </section>
   )
