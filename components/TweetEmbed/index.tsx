94246dfe0a73df12e04fe24edc6798ab72f94ff4
Josman Proudinat <jmps2812@gmail.com>
2022-05-19 15:08:55 -0600

    twitter embed with package

--- components/TweetEmbed/index.tsx
+++ components/TweetEmbed/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TwEmbed from 'react-tweet-embed'
 
 type Props = {
   tweetId: string
@@ -6,11 +7,8 @@ type Props = {
 
 export default function TweetEmbed({ tweetId }: Props) {
   return (
-    <div className='w-full flex justify-center'>
-      <blockquote className="twitter-tweet">
-        <a href={`https://twitter.com/JosmanProudinat/status/${tweetId}?ref_src=twsrc%5Etfw`}></a>
-      </blockquote>
-      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+    <div className='max-w-sm mx-auto'>
+      <TwEmbed tweetId={tweetId} />
     </div>
   )
 }
